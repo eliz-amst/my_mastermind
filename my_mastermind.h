b9ed7f0e649d790f1fb1f54dce2ea1168e51b9e8
Yelyzaveta Samodid <samodedliza@gmail.com>
2024-06-25 11:19:56 -0500

    Create my_mastermind.h

---
+++ my_mastermind.h
@@ -0,0 +1,16 @@
+#ifndef MY_MASTERMIND_H
+#define MY_MASTERMIND_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+
+#define CODE_LEN 4
+
+extern int attempts;
+extern int rounds;
+
+#endif
